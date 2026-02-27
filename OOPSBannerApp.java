61b11dde4dfe206e8be7b54e5094dbf8538525a9
ShyamShanmugam <shyamshanmugam20.07@gmail.com>
2026-02-27 21:16:59 +0530

    UC4 Use Array and Loop for OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,17 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String line1 = String.join("", "O", "   ", "O", "OOOOO", "   ", "OOOOO");
-        String line2 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
-        String line3 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
-        String line4 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
-        String line5 = String.join("", " OOO ", "OOOOO", "   ", "OOOOO");
+        String[] banner = {
+            String.join("", "O   O", "   ", "OOOO", "   ", "OOOO"),
+            String.join("", "O   O", "   ", "O   O", "   ", "O   O"),
+            String.join("", "O   O", "   ", "OOOO", "   ", "OOOO"),
+            String.join("", "O   O", "   ", "O     ", "   ", "O"),
+            String.join("", " OOO ", "   ", "O     ", "   ", "OOOO")
+        };
 
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
-
 }
 
