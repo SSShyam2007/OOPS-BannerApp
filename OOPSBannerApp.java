fd7b141689eb4fa7c80327990d6e7770aa60f61c
ShyamShanmugam <shyamshanmugam20.07@gmail.com>
2026-02-27 21:05:47 +0530

    UC3 Use String.join for OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,14 +2,17 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * *     *  ***** ");
-        System.out.println("*     * *     * *     *       *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        String line1 = String.join("", "O", "   ", "O", "OOOOO", "   ", "OOOOO");
+        String line2 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
+        String line3 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
+        String line4 = String.join("", "O", "   ", "O", "O   O", "   ", "O   O");
+        String line5 = String.join("", " OOO ", "OOOOO", "   ", "OOOOO");
 
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 
 }
