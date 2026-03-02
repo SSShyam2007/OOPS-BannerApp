acad12d13bf6adeb667527ec11a6444544eadcc8
ShyamShanmugam <shyamshanmugam20.07@gmail.com>
2026-03-02 13:38:16 +0530

    UC8 HashMap implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
-
-    // Class to store character and its pattern
-    static class CharacterPattern {
-        private char character;
-        private String[] pattern;
-
-        public CharacterPattern(char character, String[] pattern) {
-            this.character = character;
-            this.pattern = pattern;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
-
-        public char getCharacter() {
-            return character;
-        }
-    }
 
     public static void main(String[] args) {
 
-        // Pattern for O
-        CharacterPattern O = new CharacterPattern('O', new String[]{
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
                 " *** ",
                 "*   *",
                 "*   *",
@@ -32,8 +17,7 @@ public class OOPSBannerApp {
                 " *** "
         });
 
-        // Pattern for P
-        CharacterPattern P = new CharacterPattern('P', new String[]{
+        patternMap.put('P', new String[]{
                 "**** ",
                 "*   *",
                 "*   *",
@@ -43,8 +27,7 @@ public class OOPSBannerApp {
                 "*    "
         });
 
-        // Pattern for S
-        CharacterPattern S = new CharacterPattern('S', new String[]{
+        patternMap.put('S', new String[]{
                 " ****",
                 "*    ",
                 "*    ",
@@ -54,15 +37,17 @@ public class OOPSBannerApp {
                 "**** "
         });
 
-        // Banner letters
-        CharacterPattern[] banner = {O, O, P, S};
+        String word = "OOPS";
 
-        // Print banner
-        for (int i = 0; i < 7; i++) {
+        for (int row = 0; row < 7; row++) {
+
             String line = "";
 
-            for (CharacterPattern cp : banner) {
-                line = line + cp.getPattern()[i] + "  ";
+            for (int i = 0; i < word.length(); i++) {
+
+                char ch = word.charAt(i);
+                line = line + patternMap.get(ch)[row] + "  ";
+
             }
 
             System.out.println(line);
